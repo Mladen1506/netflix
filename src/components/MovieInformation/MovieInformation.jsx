ab020ef06ec05a65249335c4e459a1f84f30a785
Mladen Makivic <makivic.mladen1203@gmail.com>
2022-10-31 20:30:24 -0400

    movieInformation page updated, able to display movies by genre

--- src/components/MovieInformation/MovieInformation.jsx
+++ src/components/MovieInformation/MovieInformation.jsx
@@ -5,19 +5,21 @@ import { Link, useParams } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import axios from 'axios';
 
+import { selectGenreOrCategory } from '../../features/currentGenreOrCategory';
 import useStyles from './styles';
 import { useGetMovieQuery } from '../../services/TMDB';
-import { ClassNames } from '@emotion/react';
+import genreIcons from '../../assets/genres';
 
 const MovieInformation = () => {
   const { id } = useParams();
   const { data, isFetching, error } = useGetMovieQuery(id);
   const classes = useStyles();
+  const dispatch = useDispatch();
 
-  if(isFetching) {
+  if (isFetching) {
     return (
       <Box display='flex' justifyContent='center' alignItems='center'>
-        <CircularProgress  size='8rem' />
+        <CircularProgress size='8rem' />
       </Box>
     );
   }
@@ -33,12 +35,43 @@ const MovieInformation = () => {
   return (
     <Grid container className={classes.containerSpaceAround}>
       <Grid item sm={12} lg={4}>
-        <img 
+        <img
           className={classes.poster}
           src={`https://image.tmdb.org/t/p/w500/${data?.poster_path}`}
           alt={data?.title}
-
         />
+      </Grid>
+      <Grid item container direction='column' lg={7}>
+        <Typography variant='h3' align='center' gutterBottom>{data?.title} ({data.release_date.split('-')[0]})
+        </Typography>
+        <Typography variant='h5' align='center' gutterBottom>{data?.tagline}
+        </Typography>
+        <Grid item className={classes.containerSpaceAround}>
+          <Box display='flex' align='center'>
+            <Rating readOnly value={data.vote_average / 2} />
+            <Typography variant='subtitle1' gutterBottom style={{ marginLeft: '10px' }}>
+              {data?.vote_average} / 10
+            </Typography>
+          </Box>
+          <Typography variant='h6' align='center' gutterBottom>
+            {data?.runtime}min {data?.spoken_languages.length > 0 ? `/ ${data?.spoken_languages[0].name}` : ''}
+          </Typography>
+        </Grid>
+        <Grid item className={classes.genresContainer}>
+          {data?.genres?.map((genre) => (
+            <Link
+              key={genre.name}
+              className={classes.links}
+              to='/'
+              onClick={() => dispatch(selectGenreOrCategory(genre.id))}
+            >
+              <img src={genreIcons[genre.name.toLowerCase()]} className={classes.genreImage} height={30} />
+              <Typography color='textPrimary' variant='subtitle1'>
+                {genre?.name}
+              </Typography>
+            </Link>
+          ))}
+        </Grid>
       </Grid>
     </Grid>
   );
