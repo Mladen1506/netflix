5f21b7d21beec1eb537abca94404f7b75705bd15
Mladen Makivic <makivic.mladen1203@gmail.com>
2022-11-11 11:41:07 -0500

    implemented cardMedia, added styles to featuredMovie

--- src/components/FeaturedMovie/FeaturedMovie.jsx
+++ src/components/FeaturedMovie/FeaturedMovie.jsx
@@ -4,11 +4,23 @@ import { Link } from 'react-router-dom';
 
 import useStyles from './styles';
 
-const FeaturedMovie = () => {
+const FeaturedMovie = ({ movie }) => {
   const classes = useStyles();
 
+  if (!movie) return null;
+
   return (
-    <div>FeaturedMovie</div>
+    <Box component={Link} to={`/movie/${movie.id}`} className={classes.featuredCardContainer}>
+      <Card className={classes.card} classes={{ root: classes.cardRoot }}>
+        <CardMedia
+          media='picture'
+          alt={movie.title}
+          image={`https://image.tmdb.org/t/p/original/${movie?.backdrop_path}`}
+          title={movie.title}
+          className={classes.cardMedia}
+        />
+      </Card>
+    </Box>
   );
 };
 
